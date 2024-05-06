0609a21bdaf18717ef9a3fa6b9da9f93c26c79c7
David Malpartida <dmalpartida@gmail.com>
2024-05-06 22:36:32 +0100

    Complete changes

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,62 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateCoin(rate: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "A32A645A-1BB9-4751-AE89-73E21AD3F61E"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    var delegate: CoinManagerDelegate?
+
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if (error != nil) {
+                    delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+//                    let rate = parseJSON(safeData)
+                    if let coin = parseJSON(safeData) {
+                        let rateString = String(format: "%.2f", coin.rate)
+                        print(rateString, currency)
+                        self.delegate?.didUpdateCoin(rate: rateString, currency: currency)
+                    }
+                    
+                    // convert respond to String
+//                    let str = String(decoding: safeData, as: UTF8.self)
+//                    print(str)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ coinData: Data) -> CoinModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodeData = try decoder.decode(CoinData.self, from: coinData)
+            let coinCrypto = decodeData.asset_id_base
+            let coinCurrency = decodeData.asset_id_quote
+            let coinRate = decodeData.rate
+            
+            let coin = CoinModel(cryptocurrency: coinCrypto, currency: coinCurrency, rate: coinRate)
+            
+            return coin
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
 }
---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,15 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by David Malpartida on 22/04/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel {
+    let cryptocurrency: String
+    let currency: String
+    let rate: Double
+}
